cfcf0949e5dff474f7cd3abe423ba51bbfbc7fcf
Edenilson Fonseca Chagas <edenilsonfc@gmail.com>
2021-08-26 17:07:24 -0300

    Validation null fields 3-17

--- backend/src/main/java/com/edenilson/osapi/dtos/TechnicianDto.java
+++ backend/src/main/java/com/edenilson/osapi/dtos/TechnicianDto.java
@@ -2,6 +2,8 @@ package com.edenilson.osapi.dtos;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
 import org.hibernate.validator.constraints.br.CPF;
 
 import com.edenilson.osapi.entities.Technician;
@@ -10,10 +12,14 @@ public class TechnicianDto implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
+	@NotEmpty(message = "O campo NAME é requerido")
 	private String name;
 
 	@CPF
+	@NotEmpty(message = "O campo CPF é requerido")
 	private String cpf;
+	
+	@NotEmpty(message = "O campo PHONE é requerido")
 	private String phone;
 
 	public TechnicianDto() {
--- backend/src/main/java/com/edenilson/osapi/resources/TechnicianResource.java
+++ backend/src/main/java/com/edenilson/osapi/resources/TechnicianResource.java
@@ -5,6 +5,8 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -49,7 +51,7 @@ public class TechnicianResource implements Serializable {
 		return ResponseEntity.ok().body(listTechnicianDto);*/
 	} 
 	@PostMapping
-	public ResponseEntity<TechnicianDto> create(@RequestBody TechnicianDto objTechnicianDto){
+	public ResponseEntity<TechnicianDto> create(@Valid @RequestBody TechnicianDto objTechnicianDto){
 		Technician newObjTechnician  = technicianService.create(objTechnicianDto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObjTechnician.getId()).toUri();
 		return ResponseEntity.created(uri).build();
